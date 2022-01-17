3c306ea26b107637e18169cd4c8962f9ad7138ab
Tim Scheuermann <tim@plusmid.dev>
2022-01-17 14:31:10 +0100

    Added methods to turn tuples into slices

--- tuple/tuple.go
+++ tuple/tuple.go
@@ -5,8 +5,27 @@ type T2[T, U any] struct {
 	B U
 }
 
+func ToList2[T any](t T2[T, T]) []T {
+	return []T{t.A, t.B}
+}
+
 type T3[T, U, V any] struct {
 	A T
 	B U
 	C V
+}
+
+func ToList3[T any](t T3[T, T, T]) []T {
+	return []T{t.A, t.B, t.C}
+}
+
+type T4[T, U, V, W any] struct {
+	A T
+	B U
+	C V
+	D W
+}
+
+func ToList4[T any](t T4[T, T, T, T]) []T {
+	return []T{t.A, t.B, t.C, t.D}
 }
