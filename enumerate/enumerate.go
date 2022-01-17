206b127ba1c12716bbc3181ed6aefaaa2308fd8b
Tim Scheuermann <tim@plusmid.dev>
2022-01-17 14:30:09 +0100

    Added comments for enumerate.go

--- enumerate/enumerate.go
+++ enumerate/enumerate.go
@@ -22,6 +22,7 @@ func (e *enumMapper[T, S]) Value() S {
 	return e.f(e.e.Value())
 }
 
+// Map applies a function f to every element of the enumerable e.
 func Map[T, S any](e Enumerable[T], f func(T) S) Enumerable[S] {
 	return &enumMapper[T, S]{e, f}
 }
@@ -45,6 +46,7 @@ func (e *enumFilter[T]) Value() T {
 	return e.e.Value()
 }
 
+// Filter lets only the elements pass that f returns true for.
 func Filter[T any](e Enumerable[T], f func(T) bool) Enumerable[T] {
 	return &enumFilter[T]{e, f}
 }
@@ -62,10 +64,12 @@ func (e *enumZipper[T, S]) Value() tuple.T2[T, S] {
 	return tuple.T2[T, S]{A: e.a.Value(), B: e.b.Value()}
 }
 
+// Zip takes two enumerables and returns a enumerable with tuples of them.
 func Zip[T, S any](a Enumerable[T], b Enumerable[S]) Enumerable[tuple.T2[T, S]] {
 	return &enumZipper[T, S]{a, b}
 }
 
+// Sum reads all values from an enumerable and sums them up.
 func Sum[T Number](e Enumerable[T]) (sum T) {
 	for e.Next() {
 		sum += e.Value()
@@ -73,6 +77,7 @@ func Sum[T Number](e Enumerable[T]) (sum T) {
 	return
 }
 
+// Max reads all values from an enumerable and returns the biggest one.
 func Max[T constraints.Ordered](e Enumerable[T]) Optional[T] {
 	if !e.Next() {
 		return Failure[T]()
@@ -87,6 +92,7 @@ func Max[T constraints.Ordered](e Enumerable[T]) Optional[T] {
 	return Success(max)
 }
 
+// Min reads all values from an enumerable and returns the smallest one.
 func Min[T constraints.Ordered](e Enumerable[T]) Optional[T] {
 	if !e.Next() {
 		return Failure[T]()
@@ -101,6 +107,7 @@ func Min[T constraints.Ordered](e Enumerable[T]) Optional[T] {
 	return Success(min)
 }
 
+// Count reads all values from an enumerable and returns the number of values.
 func Count[T any](e Enumerable[T]) int {
 	i := 0
 	for e.Next() {
@@ -109,11 +116,13 @@ func Count[T any](e Enumerable[T]) int {
 	return i
 }
 
+// Drain reads all values from an enumerable.
 func Drain[T any](e Enumerable[T]) {
 	for e.Next() {
 	}
 }
 
+// Consume reads n values from an enumerable.
 func Consume[T any](e Enumerable[T], n int) {
 	for i := 0; i < n; i++ {
 		if !e.Next() {
@@ -122,6 +131,7 @@ func Consume[T any](e Enumerable[T], n int) {
 	}
 }
 
+// Histogram reads all values from an enumerable and returns a map of values and their respective counts.
 func Histogram[T comparable, C Number](e Enumerable[T]) map[T]C {
 	h := make(map[T]C)
 	for e.Next() {
@@ -130,6 +140,7 @@ func Histogram[T comparable, C Number](e Enumerable[T]) map[T]C {
 	return h
 }
 
+// Unique reads all values from an enumerable and returns a set of unique values.
 func Unique[T comparable](e Enumerable[T]) set.Set[T] {
 	s := make(set.Set[T])
 	for e.Next() {
@@ -157,8 +168,10 @@ func (e *enumMutex[T]) Value() T {
 	return e.e.Value()
 }
 
+// Synchronize makes reading the enumerable thread safe.
 func Synchronize[T any](e Enumerable[T]) Enumerable[T] {
 	if _, ok := e.(*enumMutex[T]); ok {
+		// don't wrap an existing synchronized mutex
 		return e
 	}
 
