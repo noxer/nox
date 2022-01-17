9cc7ec84883cb255f868e653a5ae11367def5750
Tim Scheuermann <tim@plusmid.dev>
2022-01-17 14:57:11 +0100

    Added comments for package maps

--- maps/maps.go
+++ maps/maps.go
@@ -8,6 +8,7 @@ import (
 	"github.com/noxer/nox/tuple"
 )
 
+// Keys returns an unsorted slice of the keys of m.
 func Keys[K comparable, V any](m map[K]V) []K {
 	keys := make([]K, 0, len(m))
 	for k := range m {
@@ -16,12 +17,14 @@ func Keys[K comparable, V any](m map[K]V) []K {
 	return keys
 }
 
+// SortedKeys returns a sorted slice of the keys of m.
 func SortedKeys[K constraints.Ordered, V any](m map[K]V) []K {
 	keys := Keys(m)
 	slice.Sort(keys)
 	return keys
 }
 
+// Values returns an unsorted slice of values of m.
 func Values[K comparable, V any](m map[K]V) []V {
 	values := make([]V, 0, len(m))
 	for _, v := range m {
@@ -30,12 +33,14 @@ func Values[K comparable, V any](m map[K]V) []V {
 	return values
 }
 
+// SortedValues returns a sorted slice of values of m.
 func SortedValues[K comparable, V constraints.Ordered](m map[K]V) []V {
 	values := Values(m)
 	slice.Sort(values)
 	return values
 }
 
+// ValuesBySortedKeys returns a slice of values of m sorted by the keys of m.
 func ValuesBySortedKeys[K constraints.Ordered, V any](m map[K]V) []V {
 	keys := SortedKeys(m)
 	values := make([]V, len(m))
@@ -45,6 +50,7 @@ func ValuesBySortedKeys[K constraints.Ordered, V any](m map[K]V) []V {
 	return values
 }
 
+// KeyValues returns an unsorted slice of Key-Value pairs.
 func KeyValues[K comparable, V any](m map[K]V) []tuple.T2[K, V] {
 	tuples := make([]tuple.T2[K, V], 0, len(m))
 	for k, v := range m {
@@ -53,12 +59,14 @@ func KeyValues[K comparable, V any](m map[K]V) []tuple.T2[K, V] {
 	return tuples
 }
 
+// SortedKeyValues returns a slice of Key-Value pairs sorted by the keys.
 func SortedKeyValues[K constraints.Ordered, V any](m map[K]V) []tuple.T2[K, V] {
 	keyValues := KeyValues(m)
 	slice.SortBy(keyValues, func(t tuple.T2[K, V]) K { return t.A })
 	return keyValues
 }
 
+// SumKeys sums up the keys of m.
 func SumKeys[K Number, V any](m map[K]V) (sum K) {
 	for k := range m {
 		sum += k
@@ -66,6 +74,7 @@ func SumKeys[K Number, V any](m map[K]V) (sum K) {
 	return
 }
 
+// SumValues sums up the values of m.
 func SumValues[K comparable, V Number](m map[K]V) (sum V) {
 	for _, v := range m {
 		sum += v
@@ -73,6 +82,7 @@ func SumValues[K comparable, V Number](m map[K]V) (sum V) {
 	return
 }
 
+// ProdKeys multiplies all the keys of m.
 func ProdKeys[K Number, V any](m map[K]V) (sum K) {
 	for k := range m {
 		sum *= k
@@ -80,6 +90,7 @@ func ProdKeys[K Number, V any](m map[K]V) (sum K) {
 	return
 }
 
+// ProdValues multiplies all the values of m.
 func ProdValues[K comparable, V Number](m map[K]V) (sum V) {
 	for _, v := range m {
 		sum *= v
