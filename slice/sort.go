fa5543f06e51728b4be070152650a5846f53555f
Tim Scheuermann <tim@plusmid.dev>
2022-01-17 15:08:32 +0100

    Added comments for sorting

--- slice/sort.go
+++ slice/sort.go
@@ -19,28 +19,27 @@ func (s sorter[T]) Swap(a, b int) {
 	s[a], s[b] = s[b], s[a]
 }
 
+// Sort a slice.
 func Sort[T constraints.Ordered](sl []T) {
 	sort.Sort(sorter[T](sl))
 }
 
-func Stable[T constraints.Ordered](sl []T) {
-	sort.Stable(sorter[T](sl))
-}
-
+// Reverse sort a slice.
 func ReverseSort[T constraints.Ordered](sl []T) {
 	sort.Sort(sort.Reverse(sorter[T](sl)))
 }
 
-func ReverseStable[T constraints.Ordered](sl []T) {
-	sort.Stable(sort.Reverse(sorter[T](sl)))
-}
-
+// Search an item in a sorted slice and return its index. If the exact item is
+// not found the index points to the place it would be.
 func Search[T constraints.Ordered](sl []T, el T) int {
 	return sort.Search(len(sl), func(i int) bool {
 		return sl[i] > el
 	})
 }
 
+// SearchBy searches a slice by using a mapping function to convert an
+// unordered type into an ordered one and returns its index. If the exact item
+// is not found the index points to the place it would be.
 func SearchBy[T any, P constraints.Ordered](sl []T, f func(T) P, el T) int {
 	ref := f(el)
 
@@ -49,18 +48,22 @@ func SearchBy[T any, P constraints.Ordered](sl []T, f func(T) P, el T) int {
 	})
 }
 
+// IsSorted checks if the slice is sorted.
 func IsSorted[T constraints.Ordered](sl []T) bool {
 	return sort.IsSorted(sorter[T](sl))
 }
 
+// IsReverseSorted checks if the slice is sorted in reverse.
 func IsReverseSorted[T constraints.Ordered](sl []T) bool {
 	return sort.IsSorted(sort.Reverse(sorter[T](sl)))
 }
 
+// IsSortedBy checks if the slice is sorted by f(e).
 func IsSortedBy[T any, P constraints.Ordered](sl []T, f func(T) P) bool {
 	return sort.IsSorted(sorterBy[T, P]{sl, f})
 }
 
+// IsReverseSorted checks if the slice is sorted in reverse by f(e).
 func IsReverseSortedBy[T any, P constraints.Ordered](sl []T, f func(T) P) bool {
 	return sort.IsSorted(sort.Reverse(sorterBy[T, P]{sl, f}))
 }
@@ -82,18 +85,24 @@ func (s sorterBy[T, P]) Swap(a, b int) {
 	s.sl[a], s.sl[b] = s.sl[b], s.sl[a]
 }
 
+// SortBy sorts the slice by f(e).
 func SortBy[T any, P constraints.Ordered](sl []T, f func(T) P) {
 	sort.Sort(sorterBy[T, P]{sl, f})
 }
 
+// StableBy stable sorts the slice by f(e). Stable means, that equal elements
+// remain in the same order.
 func StableBy[T any, P constraints.Ordered](sl []T, f func(T) P) {
 	sort.Stable(sorterBy[T, P]{sl, f})
 }
 
+// StableBy stable sorts the slice by f(e) in reverse.
 func ReverseSortBy[T any, P constraints.Ordered](sl []T, f func(T) P) {
 	sort.Sort(sort.Reverse(sorterBy[T, P]{sl, f}))
 }
 
+// ReverseStableBy stable sorts the slice by f(e) in reverse. Stable means, that equal elements
+// remain in the same order.
 func ReverseStableBy[T any, P constraints.Ordered](sl []T, f func(T) P) {
 	sort.Stable(sort.Reverse(sorterBy[T, P]{sl, f}))
 }
