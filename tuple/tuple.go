fd6520258f8f689dc702be49cb158fb29e6ddce8
Tim Scheuermann <tim@plusmid.dev>
2022-01-17 17:02:09 +0100

    Added Unpack methods for tuples

--- tuple/tuple.go
+++ tuple/tuple.go
@@ -6,6 +6,11 @@ type T2[T, U any] struct {
 	B U
 }
 
+// Unpack this tuple into individual values.
+func (t T2[T, U]) Unpack() (T, U) {
+	return t.A, t.B
+}
+
 // ToList2 converts a T2 into a slice of its elements. This can only be used for tuples with matching element types.
 func ToList2[T any](t T2[T, T]) []T {
 	return []T{t.A, t.B}
@@ -16,6 +21,11 @@ type T3[T, U, V any] struct {
 	A T
 	B U
 	C V
+}
+
+// Unpack this tuple into individual values.
+func (t T3[T, U, V]) Unpack() (T, U, V) {
+	return t.A, t.B, t.C
 }
 
 // ToList3 converts a T3 into a slice of its elements. This can only be used for tuples with matching element types.
@@ -29,6 +39,11 @@ type T4[T, U, V, W any] struct {
 	B U
 	C V
 	D W
+}
+
+// Unpack this tuple into individual values.
+func (t T4[T, U, V, W]) Unpack() (T, U, V, W) {
+	return t.A, t.B, t.C, t.D
 }
 
 // ToList4 converts a T4 into a slice of its elements. This can only be used for tuples with matching element types.
